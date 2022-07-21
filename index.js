6cdefb4e3c6a993ddc35630be0e81c2560aec861
Sulkifli Asmunandar <kflmattukz@gmail.com>
2022-07-21 16:47:41 +0800

    add router urlshortener

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -18,6 +19,17 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+// add url shortener
+app.post('/api/shorturl' , function (req,res) {
+  
+  res.json({ urlshortener : req.body.url })
+});
+
+//hit te url shortener using Id from 
+app.get('/api/shorturl/:id', function(req , res) {
+  res.json({ urlId : req.params.id })
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
