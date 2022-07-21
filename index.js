3b01905e3e526bf8661e8058f89bd087b584d142
Sulkifli Asmunandar <kflmattukz@gmail.com>
2022-07-21 19:11:18 +0800

    Finish the task 🎉😊👌

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const dns = require('dns');
+const fs = require('fs')
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -22,13 +24,67 @@ app.get('/api/hello', function(req, res) {
 
 // add url shortener
 app.post('/api/shorturl' , function (req,res) {
-  
-  res.json({ urlshortener : req.body.url })
+
+  const original_url = req.body.url
+  const object_url = new URL(original_url)
+  let short_url
+  dns.lookup(object_url.hostname , function (err, address , family) {
+    if (err) {
+      return res.json({ 'error': 'invalid url' })
+    }
+    //make new Shortener ID
+    short_url = Math.floor(Math.random() * 100000)
+    
+    let listUrls = []
+
+    try {
+      const data = fs.readFileSync('shortUrl.json', 'utf8');
+      const listUrl = JSON.parse(data)
+      listUrl.map(url => listUrls.push(url))
+    } catch (err) {
+      console.error(err);
+    }
+
+    // fs.readFile('shortUrl.json', 'utf-8', function(err ,data) {
+    //   if (err) {
+    //     return console.log(err)
+    //   }
+    //   const listUrl = JSON.parse(data)
+    //   listUrl.map(url => listUrls.push(url))
+    // })
+
+
+    listUrls.push({ original_url: original_url , short_url: short_url })
+    console.log(listUrls)
+    fs.writeFile('shortUrl.json', JSON.stringify(listUrls, null, 2) , function(err) {
+      if (err) {
+        return console.log(err)
+      }
+    } )
+
+    res.json({ 
+      'original_url' : original_url,
+      'short_url' : short_url 
+    })
+
+  })
 });
 
 //hit te url shortener using Id from 
-app.get('/api/shorturl/:id', function(req , res) {
-  res.json({ urlId : req.params.id })
+app.get('/api/shorturl/:short_url', function(req , res) {
+  
+  try {
+    const data = fs.readFileSync('shortUrl.json', 'utf8');
+    const listUrl = JSON.parse(data)
+    listUrl.map(url => {
+      if (url.short_url.toString() === req.params.short_url) {
+        res.redirect(url.original_url)
+      }
+    })
+  } catch (err) {
+    console.error(err);
+  }
+
 })
 
 app.listen(port, function() {
